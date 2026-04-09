52940b0f55250fb6d2cd9200d44fd23ea013ed8a
ademhub <26.its.fakemahrez.26@gmail.com>
2026-04-09 15:51:00 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-// Supabase client config
-// Remplace par tes vraies clés projet Supabase
-const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmZ3F6enVnZmNxd2l0YWRxZ3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2OTA5NjksImV4cCI6MjA5MTI2Njk2OX0.244shaPDNmP0YAglLe89cfh61qqvxZ-VUO0NWUdaFZ4';
-
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export default _supabase;
